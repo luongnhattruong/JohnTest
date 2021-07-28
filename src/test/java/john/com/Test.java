7a3fe1dce716df872d0da2493502fad5f0ff8c36
Truong <truong160884@gmail.com>
2021-07-29 02:02:58 +0700

    test

--- src/test/java/john/com/Test.java
+++ src/test/java/john/com/Test.java
@@ -4,6 +4,7 @@ import org.junit.BeforeClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class Test {
@@ -14,7 +15,7 @@ public class Test {
     }
     @org.junit.Test
     public void a () {
-        fail();
+        assertTrue(true);
         log.info("GCP_P12_KEYFILE: {}" , System.getenv("GCP_P12_KEYFILE"));
         System.out.println(System.getenv("GCP_P12_KEYFILE"));
     }
