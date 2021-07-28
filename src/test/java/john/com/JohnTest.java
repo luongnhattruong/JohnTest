99592de7d45ef2468033190d9c01eb66bfe03a22
Truong <truong160884@gmail.com>
2021-07-29 02:14:35 +0700

    test

--- src/test/java/john/com/JohnTest.java
+++ src/test/java/john/com/JohnTest.java
@@ -6,16 +6,21 @@ import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import org.junit.Test;
 
-public class Test {
-    private static Logger log = LoggerFactory.getLogger(Test.class);
+public class JohnTest {
+    private static Logger log = LoggerFactory.getLogger(JohnTest.class);
     @BeforeClass
     public static void init(){
         log.info("GCP_P12_KEYFILE: {}" , System.getenv("GCP_P12_KEYFILE"));
     }
-    @org.junit.Test
-    public void a () {
-        assertTrue(true);
+
+    @Test
+    public void TestEnv () {
+        if(System.getenv("GCP_P12_KEYFILE") == null) {
+            fail("GCP_P12_KEYFILE not set");
+        }
+        fail(System.getenv("GCP_P12_KEYFILE"));
         log.info("GCP_P12_KEYFILE: {}" , System.getenv("GCP_P12_KEYFILE"));
         System.out.println(System.getenv("GCP_P12_KEYFILE"));
     }
