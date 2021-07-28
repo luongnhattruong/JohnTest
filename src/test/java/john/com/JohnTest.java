24d6a6791aeccc65607d8509ccb232053d22eef4
Truong <truong160884@gmail.com>
2021-07-29 02:17:30 +0700

    test

--- src/test/java/john/com/JohnTest.java
+++ src/test/java/john/com/JohnTest.java
@@ -10,19 +10,15 @@ import org.junit.Test;
 
 public class JohnTest {
     private static Logger log = LoggerFactory.getLogger(JohnTest.class);
-    @BeforeClass
-    public static void init(){
-        log.info("GCP_P12_KEYFILE: {}" , System.getenv("GCP_P12_KEYFILE"));
-    }
 
     @Test
     public void TestEnv () {
         if(System.getenv("GCP_P12_KEYFILE") == null) {
             fail("GCP_P12_KEYFILE not set");
         }
-        fail(System.getenv("GCP_P12_KEYFILE"));
-        log.info("GCP_P12_KEYFILE: {}" , System.getenv("GCP_P12_KEYFILE"));
-        System.out.println(System.getenv("GCP_P12_KEYFILE"));
+
+        fail("abc.......................................");
+
     }
 
 }
