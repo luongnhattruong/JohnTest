44976375f1145d7343bc8bcb4ae6692d144904d1
Truong <truong160884@gmail.com>
2021-07-29 01:57:42 +0700

    test

---
+++ src/test/java/john/com/Test.java
@@ -0,0 +1,22 @@
+package john.com;
+
+import org.junit.BeforeClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.fail;
+
+public class Test {
+    private static Logger log = LoggerFactory.getLogger(Test.class);
+    @BeforeClass
+    public static void init(){
+        log.info("GCP_P12_KEYFILE: {}" , System.getenv("GCP_P12_KEYFILE"));
+    }
+    @org.junit.Test
+    public void a () {
+        fail();
+        log.info("GCP_P12_KEYFILE: {}" , System.getenv("GCP_P12_KEYFILE"));
+        System.out.println(System.getenv("GCP_P12_KEYFILE"));
+    }
+
+}
